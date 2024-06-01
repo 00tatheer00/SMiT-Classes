101a63282829ee84ea19533d93834a661f7215c7
Tatheer Hussain <tatheerabidi0@gmail.com>
2024-06-02 01:12:15 +0500

    done all

---
+++ Class01/script.js
@@ -0,0 +1,36 @@
+// CHAPTER 2
+
+
+// var userName;
+
+// userName = "Mark";
+// userName = "Jon Doe";
+
+// // alert(userName)
+
+// console.log(userName)
+
+
+// CHAPTER 3
+
+
+// var weight = 60 ;
+
+// weight = weight + 50;
+
+// console.log(weight);
+
+
+// var a = "2";
+// var b = 3;
+
+// var c = a+b;
+// console.log(c);
+
+
+
+
+// console.log("4 + 4 = " + (4 + 4))
+
+
+var num;
---
+++ Class02/script.js
@@ -0,0 +1,88 @@
+// var num = 10;
+// var popularNumber = num + 200;
+
+// console.log(popularNumber)
+
+
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+
+// console.log(popularNumber)
+
+
+
+
+// var whatsLeftOver = 10 % 3;
+// console.log(whatsLeftOver);
+
+
+// num = 5;
+
+
+// ++num
+// // num = num - 1;
+
+// console.log(num)
+
+
+
+// CHAPTER 8 - CONCATINATING TEXT STRINGS
+
+// var userName = "Mark";
+
+// alert("Thanks" + " " + userName);
+
+
+// var message =  "Thanks ";
+// var userName = "Mark ";
+// var banger = "!";
+
+// var custom = message + userName + banger;
+// alert(custom);
+
+
+// console.log(4 + 3 + 4)
+
+
+
+// CHAP 9 -  PROMPTS
+
+// var userName = prompt("Enter your name");
+
+// console.log(userName);
+
+
+// var numberOfCats = +prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
+// alert(tooManyCats)
+
+
+
+
+
+// var retrieveValue = prompt("Enter your name");
+// var message = "Thanks ";
+// var bangen = " !";
+
+// console.log(message + retrieveValue + bangen);
+
+
+
+// CHAP 10 IF STATEMENT
+
+// var x = prompt("Where does the Pope live?");
+
+
+// if (x === "Vatican") {
+//     alert("Correct!");
+// }
+// if (x !== "Vatican") {
+//     alert("Correct!");
+// }
+
+var x = 6;
+if(x===6){
+    alert("correct")
+}
